edbe61ecec11cb0825edda6890a1adefe4aabfcd
Dasnapplel <DiegoDJBV@hotmail.com>
2025-12-03 03:04:18 -0500

    FrontEnd first draft
    forecast and credits pages added
    forecast page includes about forecast, 4 day forecast, and forecast timelapse. Credits contains names resources and other info.

---
+++ main.js
@@ -0,0 +1,20 @@
+// on click a day btn, make all forecast invisible
+// make the needed one visible
+
+const buttons = document.querySelectorAll('button');
+const forecasts = document.querySelectorAll('.forecast');
+
+buttons.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+
+    // Hide all forecast items
+    forecasts.forEach(f => f.style.display = "none");
+    forecasts[index].style.display = "flex";
+
+    // Remove active class from all buttons
+    buttons.forEach(b => b.classList.remove("active"));
+
+    // Add active class to the clicked button
+    btn.classList.add("active");
+  });
+});
