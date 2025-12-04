20153c4486a5b72a3eed315019e5a727a8c176dd
Dasnapplel <DiegoDJBV@hotmail.com>
2025-12-04 16:17:12 -0500

    final changes
    everything

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 // Forecast buttons functionality
 const buttons = document.querySelectorAll('button');
-const forecasts = document.querySelectorAll('.forecast-img');
+const forecasts = document.querySelectorAll('.forecast');
+
 
 buttons.forEach((btn, index) => {
   btn.addEventListener("click", () => {
 
     // Hide all forecast items
     forecasts.forEach(f => f.style.display = "none");
-    forecasts[index].style.display = "block";
+    forecasts[index].style.display = "flex";
 
     // Remove active class from all buttons
     buttons.forEach(b => b.classList.remove("active"));
