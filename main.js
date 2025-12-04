4208c5da6160fb00a8b3d248032089690f5c57c3
Dasnapplel <DiegoDJBV@hotmail.com>
2025-12-04 15:05:09 -0500

    semifinal push
    small text changes and updates to content

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Forecast buttons functionality
 const buttons = document.querySelectorAll('button');
-const forecasts = document.querySelectorAll('.forecast');
+const forecasts = document.querySelectorAll('.forecast-img');
 
 buttons.forEach((btn, index) => {
   btn.addEventListener("click", () => {
 
     // Hide all forecast items
     forecasts.forEach(f => f.style.display = "none");
-    forecasts[index].style.display = "flex";
+    forecasts[index].style.display = "block";
 
     // Remove active class from all buttons
     buttons.forEach(b => b.classList.remove("active"));
